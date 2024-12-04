d1e545ad044c10fea22cae37a563ab79912d259a
Ahmed Al-Dhayani <bplahmed28@gmail.com>
2024-12-04 16:51:40 +0300

    style: make the perosn page use grid

--- src/app/person/page.tsx
+++ src/app/person/page.tsx
@@ -7,32 +7,32 @@ const Page = async () => {
   const data = await fetchData(`/person/popular`);
 
   return (
-    <div className="dark:bg-primary-dark dark:text-white">
+    <div className="dark:bg-primary-dark">
       <h1 className="pt-5 text-4xl text-center font-semibold">
         Popular People
       </h1>
-      <div className="p-5 flex flex-wrap justify-center container mx-auto gap-6 pt-5">
-        {data.results.map((person: any) => (
-          <Link
-            key={person.id}
-            href={`/person/${person.id}`}
-            className="bg-slate-200 rounded-2xl max-w-[250px] 
-            hover:scale-105 duration-200"
-          >
-            <div className="text-center max-w-[250px]">
-              <Image
-                width={400}
-                height={400}
-                priority
-                className="w-full h-80 object-cover rounded-t-2xl"
-                src={fetchImage(person.profile_path)}
-                // src="https://picsum.photos/400/400"
-                alt={person.name}
-              />
-              <h3 className="text-xl font-semibold pb-3">{person.name}</h3>
-            </div>
-          </Link>
-        ))}
+      <div className="p-5 container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+          {data.results.map((person: any) => (
+            <Link
+              key={person.id}
+              href={`/person/${person.id}`}
+              className="bg-slate-200 rounded-2xl hover:scale-105 duration-200"
+            >
+              <div className="text-center">
+                <Image
+                  width={400}
+                  height={400}
+                  priority
+                  className="w-full h-80 object-cover rounded-t-2xl"
+                  src={fetchImage(person.profile_path)}
+                  alt={person.name}
+                />
+                <h3 className="text-xl font-semibold pb-3">{person.name}</h3>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
