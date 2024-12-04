cb0ae9f147a62e1ed68fd5109ad0d67702080693
Ahmed Al-Dhayani <bplahmed28@gmail.com>
2024-12-04 16:54:46 +0300

    style: add some padding

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -7,7 +7,7 @@ import free from "@/data/categories/free.json";
 const HomeSection = () => {
   return (
     <div className="bg-primary dark:bg-primary-dark dark:text-white">
-      <div className="container mx-auto flex flex-col gap-16 py-10">
+      <div className="container mx-auto flex flex-col gap-16 px-3 py-10">
         <MediaSection title="Trending" categories={trending} />
         <MediaSection title="What's Popular" categories={popular} />
         <MediaSection title="Free To Watch" categories={free} />
