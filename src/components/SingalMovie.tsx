1582a09279290ca65657dcf7be936ad2855b9e73
Ahmed Al-Dhayani <bplahmed28@gmail.com>
2024-12-04 16:55:50 +0300

    feat: fetch data in SingalMovie

--- src/components/SingalMovie.tsx
+++ src/components/SingalMovie.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { fetchData } from "@/util/FetchData";
 
-const SingalMovie = () => {
+interface Props {
+  id: string;
+  route: string;
+}
+const SingalMovie = async ({ id, route }: Props) => {
+  const data = await fetchData(`/${route}/${id}`);
+  console.log(data);
+
   return <div>SingalMovie</div>;
 };
 
