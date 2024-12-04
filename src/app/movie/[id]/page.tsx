2ad7c646c1d5d8fdc701facb4e62115cd9bb7595
Ahmed Al-Dhayani <bplahmed28@gmail.com>
2024-12-04 16:50:35 +0300

    chore: add some props to singalMovie compoents

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -3,13 +3,18 @@ import SingalMovie from "@/components/SingalMovie";
 interface Props {
   id: string;
 }
+
 const page = async function Page({ params }: { params: Promise<Props> }) {
-  const id = (await params).id;
+  const { id } = await params;
   const list = ["popular", "upcoming", "now_playing", "top_rated"];
 
   return (
     <>
-      {list.includes(id) ? <ListPage id={id} route="movie" /> : <SingalMovie />}
+      {list.includes(id) ? (
+        <ListPage id={id} route="movie" />
+      ) : (
+        <SingalMovie id={id} route="movie" />
+      )}
     </>
   );
 };
